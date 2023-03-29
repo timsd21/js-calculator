ddd1fb241c8cece125aab70e87c6c6f045dcbb52
Tim Schaedlich <timschaedlich21@web.de>
2023-03-29 08:33:23 +0200

    added functionality to the division and multiplication and fixed a bug with 0

--- calc.js
+++ calc.js
@@ -21,9 +21,12 @@ const scresult = document.querySelector(".result");
 
 plus.addEventListener("click", ()=>{
     if(scentry.textContent.at(-1)!== "-" && scentry.textContent.at(-1)!== "+" && scentry.textContent.at(-1)!== "*" && scentry.textContent.at(-1)!== "÷"){
-       
-        store = store + Number(scentry.textContent);
-        scresult.textContent= scresult.textContent+ scentry.textContent +"+";
+       if(store == 0){
+        store = Number(scentry.textContent);
+       }
+        else{store = store + Number(scentry.textContent);}
+        
+        scresult.textContent= scresult.textContent+ scentry.textContent +"÷";
         scentry.textContent = "";
 
         
@@ -31,14 +34,33 @@ plus.addEventListener("click", ()=>{
 
 })
 division.addEventListener("click",()=>{
+    if(scentry.textContent.at(-1)!== "-" && scentry.textContent.at(-1)!== "+" && scentry.textContent.at(-1)!== "*" && scentry.textContent.at(-1)!== "÷"){
+        if(store == 0){
+        store = Number(scentry.textContent);        }
+        else{store = store / Number(scentry.textContent);}
+        scresult.textContent= scresult.textContent+ scentry.textContent +"÷";
+        scentry.textContent = ""; 
+    }
 })
 multiplication.addEventListener("click", ()=>{
+    if(scentry.textContent.at(-1)!== "*" && scentry.textContent.at(-1)!== "+" && scentry.textContent.at(-1)!== "-" && scentry.textContent.at(-1)!== "÷"){
+        if(store == 0){
+            store = Number(scentry.textContent);        }
+        else{
+            store = store * Number(scentry.textContent);
+        }
+        scresult.textContent= scresult.textContent+ scentry.textContent +"*";
+        scentry.textContent = ""; 
+    }
 })
 point.addEventListener("click", ()=>{
 })
 minus.addEventListener("click", ()=>{
     if(scentry.textContent.at(-1)!== "-" && scentry.textContent.at(-1)!== "+" && scentry.textContent.at(-1)!== "*" && scentry.textContent.at(-1)!== "÷"){
-        scentry.textContent = scentry.textContent + "-";
+       
+        store = store - Number(scentry.textContent);
+        scresult.textContent= scresult.textContent+ scentry.textContent +"+";
+        scentry.textContent = ""; 
     }
 })
 result.addEventListener("click", ()=>{
@@ -46,36 +68,72 @@ result.addEventListener("click", ()=>{
         store += Number(scentry.textContent);
         scresult.textContent = "";
     }
+    else if(scresult.textContent.at(-1) === "-"){
+        store -= Number(scentry.textContent);
+        scresult.textContent = "";
+    }
+    else if(scresult.textContent.at(-1) === "÷"){
+        alert(store);
+        store /= Number(scentry.textContent);
+        scresult.textContent = "";
+    }
+    else if(scresult.textContent.at(-1) === "*"){
+        store *= Number(scentry.textContent);
+        scresult.textContent = "";
+    }
+    
     scentry.textContent = store;
     store = 0;
 })
 one.addEventListener("click", ()=>{
-        scentry.textContent = scentry.textContent + "1";  
+    if(scentry.textContent == "0"){
+        scentry.textContent = "1";
+    }    
+    else{scentry.textContent = scentry.textContent + "1";}  
 })
 two.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "2";  
+    if(scentry.textContent == "0"){
+        scentry.textContent = "2";
+    }    
+    else{scentry.textContent = scentry.textContent + "2";}   
 })
 three.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "3";  
-})
+    if(scentry.textContent == "0"){
+        scentry.textContent = "3";
+    }    
+    else{scentry.textContent = scentry.textContent + "3";} })
 four.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "4";  
+    if(scentry.textContent == "0"){
+        scentry.textContent = "4";
+    }    
+    else{scentry.textContent = scentry.textContent + "4";}   
 })
 five.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "5";  
+    if(scentry.textContent == "0"){
+        scentry.textContent = "5";
+    }    
+    else{scentry.textContent = scentry.textContent + "5";}   
 })
 six.addEventListener("click", ()=>{  
-    scentry.textContent = scentry.textContent + "6";  
-})
+    if(scentry.textContent == "0"){
+        scentry.textContent = "6";
+    }    
+    else{scentry.textContent = scentry.textContent + "6";} })
 seven.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "7";  
-})
+    if(scentry.textContent == "0"){
+        scentry.textContent = "7";
+    }    
+    else{scentry.textContent = scentry.textContent + "7";} })
 eight.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "8";  
-})
+    if(scentry.textContent == "0"){
+        scentry.textContent = "8";
+    }    
+    else{scentry.textContent = scentry.textContent + "8";} })
 nine.addEventListener("click", ()=>{
-    scentry.textContent = scentry.textContent + "9";  
-})
+    if(scentry.textContent == "0"){
+        scentry.textContent = "9";
+    }    
+    else{scentry.textContent = scentry.textContent + "9";} })
 zero.addEventListener("click", ()=>{
     scentry.textContent = scentry.textContent + "0";  
 })
